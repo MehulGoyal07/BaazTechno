c97a87a40b7141bbd66616724b4d7ff4f168261b
Amber Kaushik <mbrkaushik@gmail.com>
2025-02-01 22:23:08 +0530

    added-components-in-contact-us-section

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,20 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import Navbar from '../components/Navbar';
+import ContactForm from "../components/ContactUs/ContactForm";
+import Footer from '../components/Footer';
+import SocialMediaLinks from '../components/ContactUs/SocialMediaLinks';
+import Faqs from '../components/ContactUs/FAQs';
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <>
+    <Navbar />
+    <ContactForm />
+    <Faqs />
+    <SocialMediaLinks />
+    <Footer />
+    </>
   )
 }
 

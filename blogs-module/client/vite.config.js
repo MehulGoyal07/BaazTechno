939ea4448f14117d9b0bce5c9f472294b44734d6
MehulGoyal07 <mehulgoyal107@gmail.com>
2025-04-17 21:50:29 +0530

    blogs-module-shifted

--- blogs-module/client/vite.config.js
+++
@@ -1,8 +0,0 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import flowbiteReact from "flowbite-react/plugin/vite";
-
-// https://vite.dev/config/
-export default defineConfig({
-    plugins: [react(), flowbiteReact()],
-})

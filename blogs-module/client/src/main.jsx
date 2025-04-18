0f1c610713cd4dc40c9109e56ccca1d0e8547f0a
MehulGoyal07 <mehulgoyal107@gmail.com>
2025-04-18 15:04:57 +0530

    add-redux-persist

--- blogs-module/client/src/main.jsx
+++ blogs-module/client/src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App.jsx';
-import { store } from './redux/store.js';
+import { persistor, store } from './redux/store.js';
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </PersistGate>
 );
--- blogs-module/client/src/redux/store.js
+++ blogs-module/client/src/redux/store.js
@@ -1,8 +1,29 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from './user/userSlice.js'
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import persistStore from 'redux-persist/es/persistStore';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import userReducer from './user/userSlice.js';
+
+const rootReducer = combineReducers({
+    user: userReducer,
+})
+
+const persistConfig = {
+        key: 'root',
+        storage,
+        version: 1,
+    }
+    // Note: In a real-world application, you would typically use a more secure storage solution for sensitive data.
+    // For example, you might use sessionStorage or a secure cookie storage library.
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-    reducer: {
-        user: userReducer,
-    },
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
 })
+
+export const persistor = persistStore(store)

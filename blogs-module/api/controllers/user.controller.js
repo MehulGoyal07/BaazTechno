80a9a79181b277ae3a0fabb41030197596f16cc6
MehulGoyal07 <mehulgoyal107@gmail.com>
2025-04-19 15:44:24 +0530

    delete-route-updated

--- blogs-module/api/controllers/user.controller.js
+++ blogs-module/api/controllers/user.controller.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import bcryptjs from 'bcryptjs'
 import User from '../models/user.model.js'
-import errorHandler from "../utils/error.js"
+import errorHandler from '../utils/error.js'
 export const test = (req, res) => {
     res.json({
         message: 'API is working'
@@ -20,7 +20,9 @@ export const updateUser = async(req, res, next) => {
     }
     if (req.body.username) {
         if (req.body.username.length < 7 || req.body.username.length > 20) {
-            return next(errorHandler(400, 'Username must be between 7 and 20 characters!'))
+            return next(
+                errorHandler(400, 'Username must be between 7 and 20 characters!')
+            )
         }
         if (req.body.username.includes(' ')) {
             return next(errorHandler(400, 'Username must not contain spaces!'))
@@ -31,23 +33,35 @@ export const updateUser = async(req, res, next) => {
         if (!req.body.username.match(/^[a-z0-9]+$/)) {
             return next(errorHandler(400, 'Username must be alphanumeric!'))
         }
-        try {
-            const updatedUser = await User.findByIdAndUpdate(
-                req.params.userId, {
-                    $set: {
-                        username: req.body.username,
-                        email: req.body.email,
-                        profilePicture: req.body.profilePicture,
-                        password: req.body.password
-                    }
-                }, {
-                    new: true
+    }
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.userId, {
+                $set: {
+                    username: req.body.username,
+                    email: req.body.email,
+                    profilePicture: req.body.profilePicture,
+                    password: req.body.password
                 }
-            )
-            const { password, ...others } = updatedUser._doc
-            res.status(200).json(others)
-        } catch (error) {
-            return next(error)
-        }
+            }, {
+                new: true
+            }
+        )
+        const { password, ...others } = updatedUser._doc
+        res.status(200).json(others)
+    } catch (error) {
+        return next(error)
+    }
+}
+
+export const deleteUser = async(req, res, next) => {
+    if (req.user.id !== req.params.userId) {
+        return next(errorHandler(403, 'You can only delete your account!'))
+    }
+    try {
+        await User.findByIdAndDelete(req.params.userId)
+        res.status(200).json('User has been deleted.')
+    } catch (error) {
+        return next(error)
     }
 }
--- blogs-module/client/src/redux/user/userSlice.js
+++ blogs-module/client/src/redux/user/userSlice.js
@@ -1,30 +1,50 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
     user: null,
     error: null,
-    loading: false,
-};
+    loading: false
+}
 
 const userSlice = createSlice({
     name: 'user',
     initialState,
     reducers: {
-        signInStart: (state) => {
-            state.loading = true;
-            state.error = null;
+        signInStart: state => {
+            state.loading = true
+            state.error = null
         },
         signInSuccess: (state, action) => {
-            state.currentUser = action.payload;
-            state.loading = false;
-            state.error = null;
+            state.currentUser = action.payload
+            state.loading = false
+            state.error = null
         },
         signInFailure: (state, action) => {
-            state.loading = false;
-            state.error = action.payload;
+            state.loading = false
+            state.error = action.payload
         },
-    },
-});
-export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
+        updateStart: state => {
+            state.loading = true
+            state.error = null
+        },
+        updateSuccess: (state, action) => {
+            state.currentUser = action.payload
+            state.loading = false
+            state.error = null
+        },
+        updateFailure: (state, action) => {
+            state.loading = false
+            state.error = action.payload
+        }
+    }
+})
+export const {
+    signInStart,
+    signInSuccess,
+    signInFailure,
+    updateStart,
+    updateSuccess,
+    updateFailure
+} = userSlice.actions
 
-export default userSlice.reducer;
+export default userSlice.reducer
